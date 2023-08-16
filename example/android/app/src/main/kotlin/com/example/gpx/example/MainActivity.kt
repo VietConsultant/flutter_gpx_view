a221f550d73f3f9b86aa448e130b84a196239934
Phan Kiet <phankiet.it@gmail.com>
2023-08-16 17:34:35 +0700

    Initial commit

---
+++ example/android/app/src/main/kotlin/com/example/gpx/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gpx.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/flutter_gpx_view.dart
@@ -0,0 +1,121 @@
+library flutter_gpx_view;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:xml/xml.dart' as xml;
+
+class GpxView extends StatefulWidget {
+  const GpxView({
+    super.key,
+    required this.xml,
+    this.polyColor = Colors.blueAccent,
+    this.markerIcon = const Icon(
+      Icons.location_on,
+      color: Colors.red,
+    ),
+    this.polyStrokeWidth = 4.0,
+    this.boundPadding = const EdgeInsets.all(20.0),
+  });
+
+  final String xml;
+  final Color polyColor;
+  final Icon markerIcon;
+  final double polyStrokeWidth;
+  final EdgeInsets boundPadding;
+
+  @override
+  State<GpxView> createState() => _GpxMapState();
+}
+
+class _GpxMapState extends State<GpxView> {
+  late List<LatLng> trackPoints;
+  late List<Marker> waypoints;
+
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    loadGpxData();
+  }
+
+  Future<void> loadGpxData() async {
+    if (widget.xml.isEmpty) {
+      // show error
+      debugPrint('No GPX data found');
+      return;
+    }
+    final document = xml.XmlDocument.parse(widget.xml);
+
+    trackPoints = [];
+    waypoints = [];
+
+    final trkpts = document.findAllElements('trkpt');
+    final wpts = document.findAllElements('wpt');
+
+    for (final trkpt in trkpts) {
+      final lat = double.parse(trkpt.getAttribute('lat')!);
+      final lon = double.parse(trkpt.getAttribute('lon')!);
+      trackPoints.add(LatLng(lat, lon));
+    }
+
+    for (final wpt in wpts) {
+      final lat = double.parse(wpt.getAttribute('lat')!);
+      final lon = double.parse(wpt.getAttribute('lon')!);
+      final tooltipMsg = '${wpt.findElements('name').first.text}\n'
+          '${wpt.findElements('desc').first.text}';
+      waypoints.add(
+        Marker(
+          point: LatLng(lat, lon),
+          builder: (_) => Tooltip(
+            triggerMode: TooltipTriggerMode.tap,
+            message: tooltipMsg,
+            waitDuration: const Duration(seconds: 5),
+            child: widget.markerIcon,
+          ),
+        ),
+      );
+    }
+
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _loading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : FlutterMap(
+            options: MapOptions(
+              center: trackPoints.first,
+              zoom: 10.0,
+              bounds: LatLngBounds.fromPoints(trackPoints),
+              boundsOptions: FitBoundsOptions(
+                padding: widget.boundPadding,
+              ),
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.opentopomap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+              ),
+              PolylineLayer(
+                polylines: [
+                  Polyline(
+                    points: trackPoints,
+                    strokeWidth: widget.polyStrokeWidth,
+                    color: widget.polyColor,
+                  ),
+                ],
+              ),
+              MarkerLayer(
+                markers: waypoints,
+              ),
+            ],
+          );
+  }
+}
